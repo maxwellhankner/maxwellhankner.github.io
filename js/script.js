29f57bd17f0e118be0c46b7cbdec7dcab654cd0c
Maxwell Hankner <maxwellhankner@gmail.com>
2021-02-09 15:26:38 -1000

    navbar and socialsbar created

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var darkMode = document.getElementById("input-color-switch");
+
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("active");
+  document.body.classList.toggle("dark-mode");
+});
