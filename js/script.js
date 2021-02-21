909fea026e1f599c910e9077c67ea2adc773b95c
Maxwell Hankner <maxwellhankner@gmail.com>
2021-02-20 16:36:00 -1000

    started mobile responsive

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ darkMode.addEventListener('click', function () {
   darkMode.classList.toggle('active');
   document.body.classList.toggle('dark-mode');
 });
+
+function toggleNav() {
+  console.log('toggleNav');
+  var nav = document.getElementById('nav-items-id');
+  if (nav.className === 'nav-items') {
+    nav.className += ' responsive';
+  } else {
+    nav.className = 'nav-items';
+  }
+}
