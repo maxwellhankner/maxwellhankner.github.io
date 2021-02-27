1d54a423a08888ec68d27435b7b0f064b538decc
Maxwell Hankner <maxwellhankner@gmail.com>
2021-02-26 18:58:35 -1000

    hover functionality

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ const closeNav = () => {
   var nav = document.getElementById('nav-items-id');
   nav.classList.remove('responsive');
 };
+
+// Gif Hover
+const soleImage = document.getElementById('sole-image');
+soleImage.addEventListener(
+  'mouseover',
+  () => {
+    let now = Date.now();
+    soleImage.style.backgroundImage = `url('/assets/sole-composer.gif?${now}')`;
+  },
+  false
+);
+soleImage.addEventListener(
+  'mouseout',
+  () => {
+    soleImage.style.backgroundImage = "url('/assets/sole-composer.jpg')";
+  },
+  false
+);
